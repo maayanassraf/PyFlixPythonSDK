fd92bfaad7430b91b5d6d7980fa9156452b2ad5b
maayanassraf <maayanassraf@gmail.com>
2024-08-22 18:59:30 +0300

    added jenkins build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup_args = dict(
     name='pyflix',
-    version='1.0.1',
+    version='1.0.2',
     description='Netflix SDK',
     license='MIT',
     install_requires=[
--- src/client.py
+++ src/client.py
@@ -25,3 +25,4 @@ class Client:
         }
         response = requests.get(endpoint, headers=headers)
         return response.json()
+
